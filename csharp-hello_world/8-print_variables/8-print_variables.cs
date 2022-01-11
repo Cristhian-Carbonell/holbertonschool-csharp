14708ca964340d69521a9a1966c624da35b4c1fd
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2022-01-11 16:32:31 -0500

    Print variables

---
+++ csharp-hello_world/8-print_variables/8-print_variables.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _8_print_variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "Street";
+            string str2 = "Mission";
+            int int1 = 972;
+            Console.WriteLine("{0} {1} {2}", int1, str2, str1);
+        }
+    }
+}
