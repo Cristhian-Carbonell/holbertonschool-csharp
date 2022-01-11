b4d97d2b796aaaaaddd58fe1d652975a6e1d6b79
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2022-01-11 16:25:33 -0500

    Play with strings

---
+++ csharp-hello_world/7-concat/7-concat.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _7_concat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "Hoberton";
+            string str2 = "School";
+            Console.WriteLine("Welcome to {0}", str1 + " " + str2);
+        }
+    }
+}
