7394d55b778897594f50c23f6a0be6c64b82db9f
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2022-01-11 16:20:06 -0500

    Print String

---
+++ csharp-hello_world/6-print_string/6-print_string.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+
+namespace _6_print_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = new StringBuilder("Hoberton School".Length * 3).Insert(0, "Hoberton School", 3).ToString();
+            string characters = str.Substring(0, 9);
+            Console.WriteLine("{0}\n{1}", str, characters);
+        }
+    }
+}
