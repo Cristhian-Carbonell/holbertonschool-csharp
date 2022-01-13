f5445e67c85ec634b91d11bdd0c79edaa59e088c
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2022-01-13 10:47:35 -0500

    00...99

---
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i <= 9; i++)
+                for(int j = 0; j <= 9; j++)
+                    Console.Write($"{i}{j}, ");
+            Console.Write("\n");
+        }
+    }
+}
