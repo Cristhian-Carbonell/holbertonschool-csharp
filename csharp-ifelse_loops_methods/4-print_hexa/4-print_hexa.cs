c623d073403a86f8cb77ec07da2b9c30563b622f
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2022-01-13 10:38:04 -0500

    Hexadecimal printing

---
+++ csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            while(number <= 98)
+            {
+                Console.WriteLine($"{number} = 0x{number:x}");
+                number++;
+            }
+        }
+    }
+}
