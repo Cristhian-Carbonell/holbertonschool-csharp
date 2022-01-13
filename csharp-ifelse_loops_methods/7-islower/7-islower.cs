c9597e608fab394cfb6e63bd42e206e7e54f281c
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2022-01-13 14:23:12 -0500

    islower

---
+++ csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Character
+{
+    public static bool IsLower(char c)
+    {
+        string alphabet = "abcdefghijklmnopqrstuvwxyz";
+        for(int i = 0; i < alphabet.Length; i++)
+            if(c == alphabet[i])
+                return true;
+        return false;        
+    }
+}
