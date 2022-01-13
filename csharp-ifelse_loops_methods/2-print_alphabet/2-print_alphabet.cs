0c855c1baadceff0505ec5ca0de4a63b18032b72
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2022-01-13 10:01:07 -0500

    I sometimes suffer from insomnia, And when i can't fall asleep, I play what I call the alphabet game

---
+++ csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _2_print_alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            for(int i = 0; i < alphabet.Length; i++)
+                Console.Write(alphabet[i]);
+        }
+    }
+}
