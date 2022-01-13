1b5e9918e9e00d558380454e8c3aca1cacdfd3e8
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2022-01-13 10:25:54 -0500

    When I was having that alphabet soup, I never thought that it would pay off

---
+++ csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _3_print_alphabt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            for(int i = 0; i < alphabet.Length; i++)
+            {
+                if(alphabet[i] == 'q' || alphabet[i] == 'e')
+                    continue;
+                Console.Write(alphabet[i]);
+            }
+            
+        }
+    }
+}
