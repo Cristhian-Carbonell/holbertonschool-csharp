65fdf1d34cd3b74352c52e198f4cb8c028645a87
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2022-01-25 16:35:04 -0500

    Find the max

---
+++ csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int MaxInteger(List<int> myList)
+    {
+        int number = 0;
+        int end = myList.Count - 1;
+
+        if (myList.Count == 0) {
+            Console.WriteLine("List is empty");
+            return -1;
+        }
+        for (int i = 0; i < myList.Count; i++) {
+            if (number > myList[i]) {
+                continue;
+            }
+            else {
+                number = myList[i];
+            }
+        }
+
+        return number;
+    }
+}
