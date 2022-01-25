265faaf3c36ac8d1b882256319f4e447273dbe7f
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2022-01-25 16:19:28 -0500

    Print a list of integers

---
+++ csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        int number = size - 1;
+        if (size == 0) {
+            Console.WriteLine("");
+        }
+        if (size < 0) {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        List<int> newList = new List<int>();
+        for (int i = 0; i < size; i++) {
+            newList.Add(i);
+            if (i == number) {
+                Console.WriteLine("{0}", newList[i]);
+            }
+            else {
+                Console.Write("{0} ", newList[i]);
+            }
+        }
+
+        return newList;
+    }
+}
