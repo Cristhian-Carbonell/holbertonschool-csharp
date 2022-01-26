b5d2abd31dfc0afac032b553097e92addff05c69
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2022-01-25 19:46:27 -0500

    Number of keys

---
+++ csharp-arrays_lists_dictionaries/8-number_keys/8-main.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+
+namespace _8_number_keys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> myDict = new Dictionary<string, string>();
+
+            myDict.Add("language", "C");
+            myDict.Add("track", "low level");
+            myDict.Add("school", "Holberton");
+
+            Console.WriteLine("Number of keys: {0}", Dictionary.NumberOfKeys(myDict));
+        }
+    }
+}
