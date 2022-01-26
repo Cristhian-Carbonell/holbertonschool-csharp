26c2b0b7f8c1521ecc8ecc40234acadf72454310
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2022-01-25 21:12:51 -0500

    Best score

---
+++ csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        int number = 0;
+        string name = "";
+        if (myList.Count == 0) {
+            return "None";
+        }
+        foreach (KeyValuePair<string, int> kvp in myList) {
+            if (number > kvp.Value) {
+                continue;
+            }
+            else {
+                number = kvp.Value;
+                name = kvp.Key;
+            }
+        }
+
+        return name;
+    }
+}
