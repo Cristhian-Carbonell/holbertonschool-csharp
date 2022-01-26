c29697687cbf760d1f101178ca31a4b254c28f2e
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2022-01-25 21:33:08 -0500

    Rectangular array

---
+++ csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _14_rectangular_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 5;
+            int[,] newArray = new int[5,5];
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+                    if (i == 2 && j == 2) {
+                        newArray[i,j] = 1;
+                    }
+                    else {
+                        newArray[i,j] = 0;
+                    }
+                }
+            }
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+                    if (j == size - 1) {
+                        Console.WriteLine("{0}", newArray[i,j]);
+                    }
+                    else {
+                        Console.Write("{0} ", newArray[i,j]);
+                    }
+                }
+            }
+        }
+    }
+}
